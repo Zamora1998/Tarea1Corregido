358a8b058d16563755ad37616282689029c4d0d9
Johnny Zamora <Johnny Zamora@DESKTOP-HD77QKG>
2023-10-15 16:50:16 -0600

    Add project files.

---
+++ Control/Controllers/CategoriasController.cs
@@ -0,0 +1,54 @@
+﻿using ClasesData;
+using Control.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace Control.Controllers
+{
+    public class CategoriasController : Controller
+    {
+        private RestauranteEntities Categ = new RestauranteEntities();
+
+        // GET: Categorias
+        public ActionResult Index()
+        {
+            // Aquí puedes agregar lógica para mostrar una lista de categorías en la vista.
+            var categorias = Categ.Categorias.ToList();
+            return View(categorias);
+        }
+
+        // POST: Categorias/AgregarCategoria
+        [HttpPost]
+        public ActionResult AgregarCategoria(Categoriasclasso nuevaCategoria)
+        {
+            try
+            {
+                var categoriaExistente = Categ.Categorias.FirstOrDefault(c => c.Nombre == nuevaCategoria.Nombre);
+
+                if (categoriaExistente != null)
+                {
+                    ModelState.AddModelError("Nombre", "Ya existe una categoría con ese nombre.");
+                    return View(nuevaCategoria);
+                }
+
+                var nuevaCategoriaEntity = new Categorias
+                {
+                    Nombre = nuevaCategoria.Nombre
+                };
+
+                Categ.Categorias.Add(nuevaCategoriaEntity);
+                Categ.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch (Exception)
+            {
+                ModelState.AddModelError(string.Empty, "Ocurrió un error al agregar la categoría.");
+                return View(nuevaCategoria);
+            }
+        }
+    }
+}
